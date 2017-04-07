d375fcf9ce827088794f34b3d27613ec5fa65376
Ken Walters <ken@lostghost.com>
2017-04-07 10:51:53 -0400

    make web app

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
+	http.ListenAndServe(":8080", r)
 }
